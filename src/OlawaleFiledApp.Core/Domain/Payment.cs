0a30777997623b3fa0b2e3a869787190412c4a83
lawale <lawale99@gmail.com>
2021-02-19 18:07:20 +0100

    added payment model and payload

---
+++ src/OlawaleFiledApp.Core/Domain/Payment.cs
@@ -0,0 +1,24 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace OlawaleFiledApp.Core.Domain
+{
+    public class Payment : BaseEntity
+    {
+        [Required]
+        public string CreditCardNumber { get; set; } = null!;
+        
+        [Required, StringLength(24)]
+        public string CardHolder { get; set; } = null!;
+        
+        [Required, DataType(DataType.Date)]
+        public DateTime ExpirationDate { get; set; }
+        
+        [StringLength(3)]
+        public string? SecurityCode { get; set; }
+        
+        [Required]
+        public decimal Amount { get; set; }
+    }
+}
---
+++ src/OlawaleFiledApp.Core/Models/Payloads/PaymentPayload.cs
@@ -0,0 +1,24 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using OlawaleFiledApp.Core.Models.Validations;
+
+namespace OlawaleFiledApp.Core.Models.Payloads
+{
+    public class PaymentPayload
+    {
+        [Required(ErrorMessage = "Credit Card Number Is Required"), DataType(DataType.CreditCard, ErrorMessage = "Provide A Valid Credit Card Number")]
+        public string CreditCardNumber { get; set; } = null!;
+        
+        [Required(ErrorMessage = "Card Holder Name Is Required"), StringLength(24, ErrorMessage = "Card Holder Name Should Not Be More Than 24 Characters Long")]
+        public string CardHolder { get; set; } = null!;
+        
+        [Date(ValidationComparison.Greater)]
+        public DateTime ExpirationDate { get; set; }
+        
+        [StringLength(3)]
+        public string? SecurityCode { get; set; }
+        
+        [Decimal(0,ValidationComparison.Greater)]
+        public decimal Amount { get; set; }
+    }
+}
